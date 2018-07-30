cbb2ba5e474ded3fd086bf9e9c6b4b0ca966aa42
analben <analmanakiwala@gmail.com>
2018-07-30 16:19:08 -0400

    changesInApp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
-import  RoomList  from './components/RoomList.js';      
+import  RoomList  from './components/RoomList.js';
+import  MessageList  from './components/MessageList.js';      
 
 var config = {
     apiKey: "AIzaSyAH4XTELIa9p2T_NTD7DUSWTOAnweJha14",
@@ -15,10 +16,24 @@ var config = {
   firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        activeRoom : null,
+
+      }
+    
+  }
+
+  setRoom(room){
+    this.setState({activeRoom: room});
+  }
+  
   render() {
     return (
       <div className="App">
-        <RoomList firebase={firebase} />
+        <RoomList firebase={firebase} activeRoom={this.state.activeRoom} setRoom={this.setRoom.bind(this)} />
+        <MessageList firebase={firebase} activeRoom={this.state.activeRoom} />
        
       
       </div>
